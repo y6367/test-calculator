992a71bab1a3a4b0a837123c012bb8d9cfc731bf
Joe <139933784+y6367@users.noreply.github.com>
2024-12-25 17:55:22 -0800

    Fixed bugs in code, need cleaning.

--- script.js
+++ script.js
@@ -9,37 +9,14 @@ let numberA = 0;
 let numberB = 0;
 let sign;
 
-function displayCurrent() {
-    storage.innerHTML = calculate;
-}
+
 function reset() {
     sign = undefined
     numberB = 0;
     storage.innerHTML = numberA
     calculate = numberA;
 }
-
-for (let i = 0; i < value.length; i++) {
-    value[i].addEventListener("click", () => {
-        calculate += value[i].innerHTML
-        displayCurrent()
-        if (numberA == undefined || sign == undefined) {
-            numberA += value[i].innerHTML
-        } else {
-            numberB += value[i].innerHTML
-        }
-    })
-}
-
-for (let i = 0; i < expression.length; i++) {
-    expression[i].addEventListener("click", () => {
-        calculate += expression[i].innerHTML
-        displayCurrent()
-        sign = expression[i].innerHTML
-    })
-}
-
-enter.addEventListener("click", () => {
+function doExpression() {
     if (sign == "+") {
         numberA = Number(numberA) + Number(numberB)
         reset()
@@ -53,10 +30,40 @@ enter.addEventListener("click", () => {
         numberA = Number(numberA) * Number(numberB)
         reset()
     }
+}
+
+for (let i = 0; i < value.length; i++) {
+    value[i].addEventListener("click", () => {
+        calculate += value[i].innerHTML
+        storage.innerHTML = calculate;
+        if (numberA == undefined || sign == undefined) {
+            numberA += value[i].innerHTML
+        } else {
+            numberB += value[i].innerHTML
+            console.log(numberB)
+            console.log(sign)
+        }
+    })
+}
+
+for (let i = 0; i < expression.length; i++) {
+    expression[i].addEventListener("click", () => {
+        if (sign != undefined) {
+            doExpression()
+        }
+        calculate += expression[i].innerHTML
+        storage.innerHTML = calculate;
+        sign = expression[i].innerHTML
+    })
+}
+
+enter.addEventListener("click", () => {
+    doExpression()
 })
 clear.addEventListener("click", () => {
     calculate = "";
-    numberA = undefined;
-    numberB = undefined;
-    displayCurrent();
+    numberA = 0;
+    numberB = 0;
+    sign = undefined
+    storage.innerHTML = calculate;
 })
